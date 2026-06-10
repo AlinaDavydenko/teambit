868f33da4589530031f6422014f887d6b7536a3a
Alina Davydenko <Alinadavydenko620@gmail.com>
2026-06-10 13:56:32 +0300

    security

--- app/core/security.py
+++ app/core/security.py
@@ -1,7 +1,12 @@
+from fastapi import Depends, HTTPException, status
 from jose import JWTError, jwt
+from app.db.session import get_db
+from sqlalchemy.orm import Session
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
 from app.config import ACCESS_TOKEN_EXPIRE_MINUTES, SECRET_KEY, ALGORITHM
+from app.db.models import User
+from fastapi.security import OAuth2PasswordBearer
 
 pwd_context = CryptContext(schemes=["bcrypt"])
 
@@ -33,3 +38,25 @@ def create_access_token(data: dict) -> str:
     token = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return token
+
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+
+def get_current_user(
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
+):
+    """Get token and database session and return an user"""
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        user_id: str = payload.get("user_id")
+        user = db.query(User).filter(User.user_id == user_id).first()
+        if user is None:
+            raise HTTPException(status_code=401, detail="User not found")
+        return user
+
+    except JWTError:
+
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
+        )
